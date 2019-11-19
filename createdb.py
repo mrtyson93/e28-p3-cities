dbce618d69a283c441fcf787b6326296ed24b379
mrtyson93 <mrtyson@umich.edu>
2019-11-18 21:02:53 -0500

    saving here for now

--- createdb.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-with open('uscities.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        print('\"' + row[1] + ", " + row[2] + '\",')
